9039f7cc7282e00f8dd6865a01f4b70447e16b97
Rohaky <yelagondarohak@gmail.com>
2026-02-04 00:46:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.querySelector(".btn").addEventListener("click", () => {
+  alert("Thanks for contacting Rohak!");
+});
+//sections appearing smoothly one by one
+const sections = document.querySelectorAll("section");
+
+const revealOnScroll = () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight * 0.85;
+
+    if (sectionTop < triggerPoint) {
+      section.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+
+// show first section immediately
+revealOnScroll();
+
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Message Sent Successfully ✅");
+  event.target.reset();
+}
